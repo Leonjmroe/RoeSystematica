44af1ec274cb27deddbea1f0a9ccb1009eb96cca
Leonjmroe <leonjmroe@gmail.com>
2023-12-02 13:59:39 +0000

    maker.py order price handler

--- maker.py
+++ maker.py
@@ -1,4 +1,4 @@
-from maker_modules import PriceHandler, OrderPlacer
+# from maker_modules import PriceHandler, OrderPlacer
 from binance_api import BinanceAPI
 from binance_price_ws import BinancePriceWebSocket
 from binance_order_ws import BinanceOrderWebSocket
@@ -41,6 +41,76 @@ class OrderHandler():
 
 
 
+
+class OrderPlacer():
+    def __init__(self, binance_api, price_handler, ticker, pip_spread, pip_risk):
+        self.binance_api = binance_api
+        self.price_hander = PriceHandler()
+        self.ticker = ticker
+        self.pip_risk = pip_risk
+        self.pip_spread = pip_spread
+        self.ask = None
+        self.bid = None
+        self.tick_size = None
+
+    def get_order_prices(self):
+        prices = self.price_hander.get_prices()
+        self.ask = round(prices[0] + (prices[0] * (self.pip_spread / 10000)), 1)
+        self.bid = round(prices[1] - (prices[1]* (self.pip_spread / 10000)), 1)
+
+    def get_btc_order_size(self):
+        dollar_risk = (self.pip_risk / 10000) * ((self.ask + self.bid) / 2)
+        btc_order_size = round((dollar_risk / ((self.ask + self.bid) / 2)), 3)
+        if btc_order_size < 0.001:
+            btc_order_size = 0.001
+        return btc_order_size
+
+    def get_tick_count(self, tick_data):
+        tick = 0.1 / float(tick_data)
+        if tick == 1:
+            return 1
+        else:
+            return str(tick).count('0')
+
+    def place_orders(self, init=False):
+        if init == True:
+            self.tick_size = self.get_tick_count(self.binance_api.get_tick_size(self.ticker))
+            order_prices = self.binance_api.get_last_bid_ask(self.ticker)
+            ask_px = round(float(order_prices[0]), self.tick_size)
+            bid_px = round(float(order_prices[1]), self.tick_size)
+        else:
+            self.get_order_prices()
+            ask_px = round(float(self.ask), self.tick_size)
+            bid_px = round(float(self.bid), self.tick_size)
+        order_size = self.get_btc_order_size()
+        ask_order = self.binance_api.create_order(symbol=self.ticker, side='SELL', type='LIMIT', quantity=order_size, price=ask_px)
+        bid_order = self.binance_api.create_order(symbol=self.ticker, side='BUY', type='LIMIT', quantity=order_size, price=bid_px)
+
+
+
+
+
+class PriceHandler():
+    def __init__(self):
+        self.ask = None
+        self.bid = None
+        self.activated = None
+        self.callback = None
+
+    def price_feed(self, data):
+        self.ask = float(data['a'])
+        self.bid = float(data['b'])
+        if not self.activated:
+            self.activated = True
+            if self.callback:
+                self.callback()
+
+    def get_prices(self):
+        return self.ask, self.bid 
+
+
+
+
 class MarketMakerController():
     def __init__(self, ticker, ws_price_stream, pip_spread, pip_risk, max_positions, api_key):
         self.binance_api = BinanceAPI()
@@ -52,6 +122,7 @@ class MarketMakerController():
         self.binance_order_ws = BinanceOrderWebSocket(self.listen_key())
 
     def start_price_ws(self):
+        self.price_handler.callback = self.place_orders
         self.binance_price_ws.callback = self.price_handler.price_feed
         price_thread = threading.Thread(target=self.binance_price_ws.run_forever)
         price_thread.start() 
@@ -67,12 +138,11 @@ class MarketMakerController():
         return listen_key
 
     def place_orders(self):
-        self.order_placer.place_orders(init=False)
+        self.order_placer.place_orders(init=True)
 
     def run(self):
         self.start_price_ws()
         self.start_order_ws()
-        self.place_orders()
 
 
 
